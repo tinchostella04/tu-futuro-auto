9da9dab6632057ffaf1cb7fd7661aaeee1b1d304
tinchostella04 <tinchostella04@gmail.com>
2026-06-09 00:51:36 -0300

    fix build errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../src/styles/fonts.css'
-import '../src/styles/theme.css'
+import './styles/fonts.css'
+import './styles/theme.css'
 import App from './app/App'
 
 createRoot(document.getElementById('root')!).render(
